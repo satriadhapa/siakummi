86d97f7810f129f46aa3c3a69c4478a57103380f
apollonsatria <satriadhapa10@gmail.com>
2023-11-05 09:53:21 +0700

    starter

---
+++ database/migrations/2023_11_05_022604_create_program_studis_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('program_studi', function (Blueprint $table) {
+            $table->id("no");
+            $table->string("NamaProdi");
+            $table->integer("NIM");
+            $table->string("MataKuliah");
+            $table->integer("JumlahMahasiswa");
+            $table->integer("JumlahMatkul");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('program_studi');
+    }
+};
---
+++ database/migrations/2023_11_05_023433_create_mahasiswas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id("no");
+            $table->integer("NIM");
+            $table->string("NamaLengkap");
+            $table->string("MataKuliah");
+            $table->string("NamaProdi");
+            $table->integer("JumlahMatkul");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
